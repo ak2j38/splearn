49ad2be6cad4a7e3ac4c2a7da0eab900606c060d
WooJin Park(Ader) <ak2j38@gmail.com>
2025-10-04 11:48:22 +0900

    Initial commit

---
+++ src/test/java/org/park/tobyspring/splearn/SplearnApplicationTests.java
@@ -0,0 +1,13 @@
+package org.park.tobyspring.splearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplearnApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
