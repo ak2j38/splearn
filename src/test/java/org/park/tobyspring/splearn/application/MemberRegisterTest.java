06ae0b7e5f5d8b8a57943f8f73b2a2e8e078b460
WooJin Park(Ader) <ak2j38@gmail.com>
2025-10-08 12:25:56 +0900

    test: 회원 애플리케이션 서비스 테스트

---
+++ src/test/java/org/park/tobyspring/splearn/application/MemberRegisterTest.java
@@ -0,0 +1,105 @@
+package org.park.tobyspring.splearn.application;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.park.tobyspring.splearn.application.provided.MemberRegister;
+import org.park.tobyspring.splearn.application.required.EmailSender;
+import org.park.tobyspring.splearn.application.required.MemberRepository;
+import org.park.tobyspring.splearn.domain.Email;
+import org.park.tobyspring.splearn.domain.Member;
+import org.park.tobyspring.splearn.domain.MemberFixture;
+import org.park.tobyspring.splearn.domain.MemberStatus;
+import org.springframework.test.util.ReflectionTestUtils;
+
+class MemberRegisterTest {
+
+  @Test
+  void registerTestStub() {
+    MemberRegister register = new MemberService(
+        new MemberRepositoryStub(),
+        new EmailSenderStub(),
+        MemberFixture.createPasswordEncoder()
+    );
+
+    Member member = register.register(MemberFixture.createRequest());
+
+    assertThat(member.getId()).isNotNull();
+    assertThat(member.getStatus()).isEqualTo(MemberStatus.PENDING);
+  }
+
+  @Test
+  void registerTestMock() {
+    EmailSenderMock emailSenderMock = new EmailSenderMock();
+    MemberRegister register = new MemberService(
+        new MemberRepositoryStub(),
+        emailSenderMock,
+        MemberFixture.createPasswordEncoder()
+    );
+
+    Member member = register.register(MemberFixture.createRequest());
+
+    assertThat(member.getId()).isNotNull();
+    assertThat(member.getStatus()).isEqualTo(MemberStatus.PENDING);
+
+    assertThat(emailSenderMock.tos).hasSize(1);
+    assertThat(emailSenderMock.tos.getFirst()).isEqualTo(member.getEmail());
+  }
+
+  @Test
+  void registerTestMockito() {
+    EmailSenderMock emailSenderMock = Mockito.mock(EmailSenderMock.class);
+    MemberRegister register = new MemberService(
+        new MemberRepositoryStub(),
+        emailSenderMock,
+        MemberFixture.createPasswordEncoder()
+    );
+
+    Member member = register.register(MemberFixture.createRequest());
+
+    assertThat(member.getId()).isNotNull();
+    assertThat(member.getStatus()).isEqualTo(MemberStatus.PENDING);
+
+    Mockito.verify(emailSenderMock).send(eq(member.getEmail()), any(), any());
+  }
+
+  static class MemberRepositoryStub implements MemberRepository {
+
+    @Override
+    public Member save(Member member) {
+      ReflectionTestUtils.setField(member, "id", 1L);
+      return member;
+    }
+  }
+
+  static class MemberRepositoryMock implements MemberRepository {
+
+    @Override
+    public Member save(Member member) {
+      ReflectionTestUtils.setField(member, "id", 1L);
+      return member;
+    }
+  }
+
+  static class EmailSenderStub implements EmailSender {
+
+    @Override
+    public void send(Email email, String subject, String body) {
+    }
+  }
+
+  static class EmailSenderMock implements EmailSender {
+
+    List<Email> tos = new ArrayList<>();
+
+    @Override
+    public void send(Email email, String subject, String body) {
+      tos.add(email);
+    }
+  }
+}
