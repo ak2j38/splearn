cec9977e5209553b22dd5e62e19a42c2b28c2192
WooJin Park(Ader) <ak2j38@gmail.com>
2025-10-09 12:57:20 +0900

    feat: equals and hashcode 구현

--- src/main/java/org/park/tobyspring/splearn/domain/AbtractEntity.java
+++ src/main/java/org/park/tobyspring/splearn/domain/AbtractEntity.java
@@ -4,9 +4,11 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 @MappedSuperclass
+@EqualsAndHashCode(of = "id")
 public class AbtractEntity {
 
   @Id
